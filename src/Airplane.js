9ab978ab024acc03141829b9681ef55f79755cdb
Atikur Rahaman <atikurahad@gmail.com>
2023-02-04 00:46:10 +0600

    some constume are changed

--- src/Airplane.js
+++ src/Airplane.js
@@ -2,14 +2,14 @@ import React from 'react'
 
 export default function Airplane(props) {
 
- const { image, title, price, description } = props.biman;
+ const { image, title, price } = props.biman;
   return (
     <>
-      <div className=''>
-        <img className='h-[30rem] w-[25rem]' src={image} alt="airways logo" />
+      <div className="border-solid border-2 border-sky-500  rounded-lg	h-[25rem] w-[20rem]	grid justify-items-center	bg-dark">
+        <img className="h-[15rem] w-[12rem] p-2" src={image} alt="airways logo" />
         <h1> Name:{title} </h1>
-        <p>{description}</p>
-        <p>{price}</p>
+        {/* <p>{description}</p> */}
+        <p className="m-2 p-2 text-2xl">Price : ${price}</p>
       </div>
     </>
   );
